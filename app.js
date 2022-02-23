f519d1a84c94019bcbf1cc3ff4e90b6145755452
Sandra Margol <sandramargol@Sandras-MacBook-Air.local>
2022-02-23 15:37:46 +0000

    styling

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ function addTodo (e){
     //delete todo
     if(item.classList[0] === 'trashBtn'){
         const todo=item.parentElement;
+        todo.classList.add('animation')
         
-        
-        todo.remove();
+        // todo.remove();
     }
     //mark to do as completed
     if(item.classList[0]=== 'completedBtn'){
